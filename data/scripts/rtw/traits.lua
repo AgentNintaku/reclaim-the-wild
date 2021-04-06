f5596abbe2561e8e63022ec6e2e6b6728e8fcec1
Nintaku <81901124+AgentNintaku@users.noreply.github.com>
2021-04-06 14:07:18 -0700

    Category and Minimum Value
    Created Power{}, Wisdom{}, and Courage{} to store traits in, and set all traits at 1 by default.

--- data/scripts/rtw/traits.lua
+++ data/scripts/rtw/traits.lua
@@ -1,31 +1,37 @@
 -- The goal here is to create a set of character statistics that can be called and used for other elements of the game, such as determining max_life and a set armor rating. These should be able to apply to hero, enemy, and NPC entities.
 
 -- Power Traits
-trait_combat = {}
-trait_hearts = {}
-trait_athletics = {}
-trait_civilization = {}
-trait_fortitude = {}
-trait_intimidate = {}
-trait_mechanics = {}
-trait_smithing = {}
+power = {
+combat = "1"
+hearts = "1"
+athletics = "1"
+civilization = "1"
+fortitude = "1"
+intimidate = "1"
+mechanics = "1"
+smithing = "1"
+}
 
 -- Wisdom Traits
-trait_willpower = {}
-trait_magic = {}
-trait_arcana = {}
-trait_perception = {}
-trait_influence = {}
-trait_perform = {}
-trait_discipline = {}
-trait_enchanting = {}
+wisdom = {
+willpower = "1"
+magic = "1"
+arcana = "1"
+perception = "1"
+influence = "1"
+perform = "1"
+discipline = "1"
+enchanting = "1"
+}
 
 -- Courage Traits
-trait_accuracy = {}
-trait_stamina = {}
-trait_nature = {}
-trait_agility = {}
-trait_command = {}
-trait_insight = {}
-trait_guile = {}
-trait_cooking = {}
+courage = {
+accuracy = "1"
+stamina = "1"
+nature = "1"
+agility = "1"
+command = "1"
+insight = "1"
+guile = "1"
+cooking = "1"
+}
